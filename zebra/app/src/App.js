712c87cc3dc010b99d13488c1483c983d2207081
Thomas Perale <thomas.perale@openmailbox.org>
2018-10-07 17:10:42 +0200

    [add] the "Parts" screen

--- zebra/app/src/App.js
+++ zebra/app/src/App.js
@@ -1,47 +1,25 @@
 import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
 import Navbar from './component/Navbar.js';
+import Home from './screens/Home';
+import Parts from './screens/Parts';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <span>
-        <Navbar/>
+      <Router>
+        <div>
+          <Navbar/>
 
-        <div className="container">
-          <h1>Separtech Printer page</h1>
-          <div className="card manual-input-card">
-            <div className="card-header">
-              Manual printing form
-            </div>
-            <div className="card-body">
-              <form action="/print" className="form-inline" method="post">
-                <div className="form-group mb-2">
-                  <label for="barcodeInput">Barcode Input</label>
-                  <input type="text" name="barcode" className="form-control" id="barcodeInput" aria-describedby="barcodeHelp" placeholder="Enter barcode"/>
-                </div>
-                <div className="form-group mb-2">
-                  <input type="text" name="name" className="form-control" id="nameInput" placeholder="Enter part name"/>
-                </div>
-                <div className="mb-2">
-                  <input type="number" name="number" className="form-control" step="1" placeholder="Number"/>
-                </div>
-                <button type="submit" className="btn btn-primary mb-2">Print</button>
-              </form>
-            </div>
-          </div>
-          <div className="card code-update-card">
-            <div className="card-header">
-              In case of fire
-            </div>
-            <div className="card-body">
-              <form action="/update" className="form-inline" method="post">
-                <button type="submit" className="btn btn-primary mb-2">Update Code</button>
-              </form>
-            </div>
-          </div>
+          <Route exact path="/" component={Home}/>
+          <Route path="/parts" component={Parts}/>
         </div>
-      </span>
+      </Router>
     );
   }
 }
