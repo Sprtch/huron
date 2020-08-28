b165c79f23b2629c46705f9ab2ef8264dae5f8a8
tperale <perale.thomas@gmail.com>
2020-08-28 23:36:56 +0200

    [fix] manage.py

--- manage.py
+++ manage.py
@@ -23,8 +23,7 @@ def make_shell_context():
     """ Creates a python REPL with several default imports
         in the context of the app
     """
-
-    return dict(app=app, db=db, User=User)
+    return dict(app=app, db=db)
 
 
 @manager.command
