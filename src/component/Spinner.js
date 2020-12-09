f2af566fe250fd13853dcd1322e9ad10c94695bf
Thomas Perale <perale.thomas@gmail.com>
2020-12-09 22:52:54 +0100

    fix spinner

--- src/component/Spinner.js
+++ src/component/Spinner.js
@@ -2,9 +2,9 @@ import React from "react";
 import styled from "styled-components";
 
 const _Loading = () => (
-  <div class="d-flex justify-content-center">
-    <div class="spinner-border text-primary" role="status">
-      <span class="sr-only">Loading...</span>
+  <div className="d-flex justify-content-center">
+    <div className="spinner-border text-primary" role="status">
+      <span className="sr-only">Loading...</span>
     </div>
   </div>
 );
