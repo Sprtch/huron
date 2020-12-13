48f54148d782f22f8a9862786cf4763df671cfd9
Thomas Perale <perale.thomas@gmail.com>
2020-12-13 17:08:25 +0100

    improved style for inventory

--- src/screens/Inventory.js
+++ src/screens/Inventory.js
@@ -5,6 +5,7 @@ import { Loading } from "../component/Spinner";
 import { CardHeaderSearch } from "../component/Card";
 import { PlainInput } from "../component/Input";
 import { Column, Table, AutoSizer } from "react-virtualized";
+import { TableWrapper } from "../component/Table";
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 
 const AddPartModal = ({ inventory, create }) => {
@@ -39,7 +40,7 @@ const AddPartModal = ({ inventory, create }) => {
   return (
     <span>
       <Button color="light" className="mr-2" onClick={toggle}>
-        Import Part
+        {"➕"}
       </Button>
       <Modal size="lg" isOpen={modal} toggle={toggle}>
         <ModalHeader toggle={toggle}>
@@ -54,44 +55,36 @@ const AddPartModal = ({ inventory, create }) => {
                   onChange={(ev) => setFilter(ev.target.value)}
                 />
 
-                <div style={{ height: 400 }}>
-                  <AutoSizer>
-                    {({ height, width }) => (
-                      <Table
-                        width={width}
-                        height={height}
-                        headerHeight={20}
-                        rowHeight={50}
-                        rowCount={
-                          exclude(
-                            ctx.filter(filter),
-                            inventory.map((x) => x.part.id)
-                          ).length
-                        }
-                        rowGetter={({ index }) =>
-                          exclude(
-                            ctx.filter(filter),
-                            inventory.map((x) => x.part.id)
-                          )[index]
-                        }
-                      >
-                        <Column label="Barcode" dataKey="barcode" width={250} />
-                        <Column width={500} label="Name" dataKey="name" />
-                        <Column
-                          width={100}
-                          label="Print"
-                          dataKey="id"
-                          cellRenderer={({ cellData }) => (
-                            <AddCell
-                              create={() => create(cellData)}
-                              id={cellData}
-                            />
-                          )}
-                        />
-                      </Table>
+                <TableWrapper
+                  size="40vh"
+                  rows={exclude(
+                    ctx.filter(filter),
+                    inventory.map((x) => x.part.id)
+                  )}
+                  rowCount={
+                    exclude(
+                      ctx.filter(filter),
+                      inventory.map((x) => x.part.id)
+                    ).length
+                  }
+                  rowGetter={({ index }) =>
+                    exclude(
+                      ctx.filter(filter),
+                      inventory.map((x) => x.part.id)
+                    )[index]
+                  }
+                >
+                  <Column label="Barcode" dataKey="barcode" width={250} />
+                  <Column width={500} label="Name" dataKey="name" />
+                  <Column
+                    width={100}
+                    label="Print"
+                    dataKey="id"
+                    cellRenderer={({ cellData }) => (
+                      <AddCell create={() => create(cellData)} id={cellData} />
                     )}
-                  </AutoSizer>
-                </div>
+                  />
+                </TableWrapper>
               </span>
             )}
           </PartContext.Consumer>
@@ -110,106 +103,106 @@ const DownloadButton = () => {
   return (
     <a href="/api/inventory/export.csv">
       <button type="button" className="btn btn-light mr-2">
-        Export to CSV
+        {"📩"}
       </button>
     </a>
-  );
-};
-
-const InventoryLine = ({ id, part, quantity, edit }) => {
-  const Quantity = ({ quantity }) => {
-    const [editing, setEditing] = useState(false);
-    const [qquantity, setQuantity] = useState(quantity);
-
-    if (editing) {
-      return (
-        <div className="btn-group mr-2" role="group" aria-label="invquantity">
-          <PlainInput
-            type="number"
-            value={qquantity}
-            onChange={(ev) => setQuantity(ev.target.value)}
-          />
-          <button
-            type="button"
-            className="btn btn-secondary"
-            onClick={() =>
-              edit({ id, quantity: qquantity }).then(() => setEditing(false))
-            }
-          >
-            Save
-          </button>
-          <button
-            type="button"
-            className="btn btn-light"
-            onClick={() => setEditing(false)}
-          >
-            X
-          </button>
-        </div>
-      );
-    } else {
-      return (
-        <span>
-          {qquantity}{" "}
-          <button className="btn btn-link" onClick={() => setEditing(true)}>
-            edit
-          </button>
-        </span>
-      );
-    }
-  };
-
-  return (
-    <tr>
-      <td>{part.barcode}</td>
-      <td>{part.name}</td>
-      <td>
-        <Quantity quantity={quantity} />
-      </td>
-    </tr>
   );
 };
 
 export default () => {
   const [filter, setFilter] = useState("");
 
+  const QuantityCell = ({ quantity, edit }) => {
+    const Quantity = ({ quantity }) => {
+      const [editing, setEditing] = useState(false);
+      const [qquantity, setQuantity] = useState(quantity);
+
+      if (editing) {
+        return (
+          <div className="btn-group mr-2" role="group" aria-label="invquantity">
+            <PlainInput
+              type="number"
+              value={qquantity}
+              onChange={(ev) => setQuantity(ev.target.value)}
+            />
+            <button
+              type="button"
+              className="btn btn-secondary"
+              onClick={() => edit(qquantity).then(() => setEditing(false))}
+            >
+              Save
+            </button>
+            <button
+              type="button"
+              className="btn btn-light"
+              onClick={() => setEditing(false)}
+            >
+              X
+            </button>
+          </div>
+        );
+      } else {
+        return (
+          <span>
+            {qquantity}{" "}
+            <button className="btn btn-link" onClick={() => setEditing(true)}>
+              edit
+            </button>
+          </span>
+        );
+      }
+    };
+
+    return <Quantity quantity={quantity} />;
+  };
+
   return (
     <InventoryContext.Consumer>
       {(ctx) => (
-        <div className="container">
-          <h1>
-            Inventory{" "}
-            <button onClick={ctx.fetch} className="btn btn-link">
-              ↻
-            </button>
-          </h1>
-
+        <div>
           <CardHeaderSearch
             value={filter}
             onChange={(ev) => setFilter(ev.target.value)}
           >
             <DownloadButton />
             <AddPartModal inventory={ctx.inventory} create={ctx.create} />
+            <Button color="secondary" onClick={ctx.fetch}>
+              ↻
+            </Button>
           </CardHeaderSearch>
           {ctx.loadingInventory ? (
             <Loading />
           ) : (
-            <table className="table table-striped">
-              <thead>
-                <tr>
-                  <th scope="col">Barcode</th>
-                  <th scope="col">Name</th>
-                  <th style={{ width: "220px" }} scope="col">
-                    Quantity
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                {ctx.filter(filter).map((x) => (
-                  <InventoryLine {...x} edit={ctx.edit} key={x.id} />
-                ))}
-              </tbody>
-            </table>
+            <TableWrapper
+              size="84vh"
+              rows={ctx.filter(filter)}
+              rowCount={ctx.filter(filter).length}
+              rowGetter={({ index }) => ctx.filter(filter)[index]}
+            >
+              <Column label="#" dataKey="id" width={50} />
+              <Column
+                width={200}
+                label="Barcode"
+                cellDataGetter={({ rowData }) => rowData.part.barcode}
+                style={{ display: "flex", alignItems: "center" }}
+              />
+              <Column
+                cellDataGetter={({ rowData }) => rowData.part.name}
+                width={400}
+                label="Name"
+              />
+              <Column
+                width={200}
+                label="Print"
+                dataKey="quantity"
+                cellRenderer={({ cellData, rowData }) => (
+                  <QuantityCell
+                    edit={(quantity) => ctx.edit({ id: rowData.id, quantity })}
+                    quantity={cellData}
+                  />
+                )}
+              />
+            </TableWrapper>
           )}
         </div>
       )}
