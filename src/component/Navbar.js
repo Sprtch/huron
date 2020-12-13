4adf84d13b1bd3149e57fee7f4147a7f0f0f5d76
Thomas Perale <perale.thomas@gmail.com>
2020-12-13 14:47:52 +0100

    better barcode handling with reactstrap

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,41 +1,25 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Navbar, NavbarBrand, Nav, NavLink, NavItem } from "reactstrap";
+import { Link, NavLink as RRNavLink } from "react-router-dom";
 
 export default () => (
-  <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-    <Link className="navbar-brand" to="/">
-      Separtech Printing Page
-    </Link>
-    <button
-      className="navbar-toggler"
-      type="button"
-      data-toggle="collapse"
-      data-target="#navbarsExampleDefault"
-      aria-controls="navbarsExampleDefault"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span className="navbar-toggler-icon"></span>
-    </button>
-
-    <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-          <Link className="nav-link" to="/">
-            Home
-          </Link>
-        </li>
-        <li className="nav-item active">
-          <Link className="nav-link" to="/parts">
+  <div>
+    <Navbar color="dark" dark expand="md">
+      <NavbarBrand tag={Link} href="/">
+        Separtch Printing Page
+      </NavbarBrand>
+      <Nav className="mr-auto" navbar>
+        <NavItem>
+          <NavLink tag={RRNavLink} to="/parts">
             Parts
-          </Link>
-        </li>
-        <li className="nav-item active">
-          <Link className="nav-link" to="/inventory">
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink tag={RRNavLink} to="/inventory">
             Inventory
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </nav>
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
+  </div>
 );
