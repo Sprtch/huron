e7066af4c689994689a3f9e55e7b4bc6f356cd41
Thomas Perale <perale.thomas@gmail.com>
2021-01-08 23:36:28 +0100

    export time since to a single file

--- src/component/Printer/Card.js
+++ src/component/Printer/Card.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { PrinterType, DialectType } from "../../models/Printer";
+import { timeSince } from "../../utils/datetime";
 import {
   Button,
   Row,
@@ -26,32 +27,6 @@ const Field = ({ title, content }) => (
     </Row>
   </ListGroupItem>
 );
-
-const timeSince = (date) => {
-  var seconds = Math.floor((new Date() - date) / 1000);
-  var interval = seconds / 31536000;
-
-  if (interval > 1) {
-    return Math.floor(interval) + " years";
-  }
-  interval = seconds / 2592000;
-  if (interval > 1) {
-    return Math.floor(interval) + " months";
-  }
-  interval = seconds / 86400;
-  if (interval > 1) {
-    return Math.floor(interval) + " days";
-  }
-  interval = seconds / 3600;
-  if (interval > 1) {
-    return Math.floor(interval) + " hours";
-  }
-  interval = seconds / 60;
-  if (interval > 1) {
-    return Math.floor(interval) + " minutes";
-  }
-  return Math.floor(seconds) + " seconds";
-};
 
 export const PrinterCard = ({
   id,
