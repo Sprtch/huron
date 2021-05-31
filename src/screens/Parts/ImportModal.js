6937ed6dfa14858e247cace4724b0aa71862791b
Thomas Perale <perale.thomas@gmail.com>
2021-05-31 17:04:02 +0200

    division of the components of the "parts" screen

---
+++ src/screens/Parts/ImportModal.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Tooltip,
+} from "reactstrap";
+import { FormGroup, Label, Input } from "reactstrap";
+
+export default ({ add }) => {
+  const [modal, setModal] = useState(false);
+  const [barcode, setBarcode] = useState("");
+  const [name, setName] = useState("");
+  const [tooltipOpen, setTooltipOpen] = useState(false);
+
+  const toggleTooltip = () => setTooltipOpen(!tooltipOpen);
+  const toggle = () => setModal(!modal);
+
+  const send = (_) => {
+    add({ barcode, name }).then(() => {
+      setBarcode("");
+      setName("");
+      setModal(false);
+    });
+  };
+
+  return (
+    <span>
+      <Button
+        color="light"
+        className="mr-2"
+        onClick={toggle}
+        id="Tooltip-add-part"
+      >
+        ➕
+      </Button>
+      <Tooltip
+        placement="top"
+        isOpen={tooltipOpen}
+        target="Tooltip-add-part"
+        toggle={toggleTooltip}
+      >
+        Add part manually to the database
+      </Tooltip>
+
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>{"Create new single part"}</ModalHeader>
+        <ModalBody>
+          <FormGroup className="m-3">
+            <Label>
+              <Input onChange={(ev) => setBarcode(ev.target.value)} />
+              Enter the barcode
+            </Label>
+          </FormGroup>
+          <FormGroup className="m-3">
+            <Label>
+              <Input onChange={(ev) => setBarcode(ev.target.value)} />
+              Enter the part name
+            </Label>
+          </FormGroup>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="warning" onClick={toggle}>
+            Close
+          </Button>
+          <Button
+            color="primary"
+            disabled={!(name.length || barcode.length)}
+            onClick={send}
+          >
+            Submit
+          </Button>
+        </ModalFooter>
+      </Modal>
+    </span>
+  );
+};
--- src/screens/Parts/Parts.js
+++ src/screens/Parts/Parts.js
@@ -1,350 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { PrinterContext } from "models/Printer";
 import { PlainInput } from "component/Input";
 import { Loading } from "component/Spinner";
 import { TableWrapper } from "component/Table";
 import { CardHeaderSearch } from "component/Card";
 import { RefreshButton } from "component/Button";
+import BulkImportModal from "./BulkImportModal";
+import ImportModal from "./ImportModal";
 import { Column } from "react-virtualized";
-import {
-  Button,
-  Container,
-  Collapse,
-  Modal,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Tooltip,
-  Row,
-  Col,
-} from "reactstrap";
-import { Form, FormGroup, Label, Input, FormText } from "reactstrap";
-import {
-  UncontrolledButtonDropdown,
-  DropdownMenu,
-  DropdownItem,
-  DropdownToggle,
-} from "reactstrap";
-
-const PartImportModal = ({ add }) => {
-  const [modal, setModal] = useState(false);
-  const [barcode, setBarcode] = useState("");
-  const [name, setName] = useState("");
-  const [tooltipOpen, setTooltipOpen] = useState(false);
-
-  const toggleTooltip = () => setTooltipOpen(!tooltipOpen);
-  const toggle = () => setModal(!modal);
-
-  const send = (_) => {
-    add({ barcode, name }).then(() => {
-      setBarcode("");
-      setName("");
-      setModal(false);
-    });
-  };
-
-  return (
-    <span>
-      <Button
-        color="light"
-        className="mr-2"
-        onClick={toggle}
-        id="Tooltip-add-part"
-      >
-        ➕
-      </Button>
-      <Tooltip
-        placement="top"
-        isOpen={tooltipOpen}
-        target="Tooltip-add-part"
-        toggle={toggleTooltip}
-      >
-        Add part manually to the database
-      </Tooltip>
-
-      <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader toggle={toggle}>{"Create new single part"}</ModalHeader>
-        <ModalBody>
-          <div className="input-group">
-            <input
-              value={barcode}
-              className="form-control"
-              placeholder="Enter barcode"
-              onChange={(ev) => setBarcode(ev.target.value)}
-            />
-            <input
-              value={name}
-              className="form-control"
-              placeholder="Enter part name"
-              onChange={(ev) => setName(ev.target.value)}
-            />
-          </div>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="warning" onClick={toggle}>
-            Close
-          </Button>
-          <Button
-            color="primary"
-            disabled={!(name.length || barcode.length)}
-            onClick={send}
-          >
-            Submit
-          </Button>
-        </ModalFooter>
-      </Modal>
-    </span>
-  );
-};
-
-const BulkImportModal = ({ importCSV }) => {
-  const DEFAULT_HEADER_BARCODE = "Code Barre";
-  const DEFAULT_HEADER_NAME = "Référence interne";
-
-  // File
-  const [fileInput, setFileInput] = useState(null);
-  const [headersName, setHeadersName] = useState([]);
-
-  // UI
-  const [modal, setModal] = useState(false);
-  const [tooltipOpen, setTooltipOpen] = useState(false);
-  const [showOption, setShowOption] = useState(false);
-
-  // Options
-  const [csvEncoding, setCsvEncoding] = useState("UTF-8");
-  const [withHeader, setWithHeader] = useState(true);
-  const [usedHeader, setUsedHeader] = useState({
-    barcode: DEFAULT_HEADER_BARCODE,
-    name: DEFAULT_HEADER_NAME,
-  });
-
-  const send = (_) => {
-    importCSV(fileInput, usedHeader.name, usedHeader.barcode, csvEncoding)
-      .then((_) => {
-        setModal(false);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-
-  useEffect(() => {
-    const parseCSV = (csv) => {
-      const arrayCSV = csv.split("\r\n");
-      if (withHeader) {
-        const headers = arrayCSV[0].split(",").map((x) => x.replace(/\"/g, ""));
-        if (headers.length < 2) {
-          console.error(
-            "Imported CSV has less than two column this error is not yet handled for the user"
-          );
-          return;
-        }
-
-        setUsedHeader({
-          barcode: headers.includes(DEFAULT_HEADER_BARCODE)
-            ? DEFAULT_HEADER_BARCODE
-            : headers[0],
-          name: headers.includes(DEFAULT_HEADER_NAME)
-            ? DEFAULT_HEADER_NAME
-            : headers[1],
-        });
-        setHeadersName(headers);
-      }
-    };
-
-    if (fileInput) {
-      const reader = new FileReader();
-      reader.readAsText(fileInput, csvEncoding);
-      reader.onload = (evt) => {
-        parseCSV(evt.target.result);
-      };
-    }
-  }, [fileInput, csvEncoding]);
-
-  const onFileInput = (e) => {
-    const input = e.target.files[0];
-    if (input) {
-      setFileInput(input);
-    }
-  };
-
-  const toggleModal = () => setModal(!modal);
-
-  const MatchComponent = ({ match }) => (
-    <span style={{ color: match ? "green" : "red" }}>{"•"}</span>
-  );
-
-  return (
-    <span>
-      <Button
-        color="light"
-        className="mr-2"
-        onClick={toggleModal}
-        id="Tooltip-import"
-      >
-        📂
-      </Button>
-      <Tooltip
-        placement="top"
-        isOpen={tooltipOpen}
-        target="Tooltip-import"
-        toggle={() => setTooltipOpen(!tooltipOpen)}
-      >
-        Import part from .csv file
-      </Tooltip>
-      <Modal isOpen={modal} toggle={toggleModal}>
-        <ModalHeader toggle={toggleModal}>
-          {"Bulk part import from .csv file"}
-        </ModalHeader>
-        <ModalBody>
-          <Form>
-            <input type="hidden" name="barcode" />
-            <input type="hidden" name="name" />
-            <FormGroup row>
-              <Col>
-                <Input
-                  id="file"
-                  type="file"
-                  name="file"
-                  onChange={onFileInput}
-                />
-                <FormText color="muted">Add you part .csv here</FormText>
-              </Col>
-            </FormGroup>
-          </Form>
-        </ModalBody>
-        <ModalBody>
-          <Button color="link" onClick={() => setShowOption(!showOption)}>
-            {showOption ? "▼" : "►"} Options
-          </Button>
-          <Collapse isOpen={showOption}>
-            <hr />
-            <legend>Header Setting</legend>
-            <div className="text-muted">
-              If your .csv file input contains the header information as the
-              first file this box should be checked
-            </div>
-            <FormGroup check inline className="m-3">
-              <Label check>
-                <Input
-                  type="checkbox"
-                  checked={withHeader}
-                  onChange={() => setWithHeader(!withHeader)}
-                />
-                Your .csv file contain an header line
-              </Label>
-            </FormGroup>
-            <legend>Encoding Setting</legend>
-            <div className="text-muted"></div>
-            <FormGroup>
-              <Label>
-                <Input
-                  type="select"
-                  value={csvEncoding}
-                  onChange={(e) => setCsvEncoding(e.target.value)}
-                >
-                  <option value="UTF-8">UTF-8</option>
-                  <option value="latin1">latin1</option>
-                </Input>
-                .csv encoding selection
-              </Label>
-            </FormGroup>
-            {fileInput && withHeader ? (
-              <div>
-                <legend>CSV column matching</legend>
-                <div className="text-muted">
-                  The .csv file you are sending should contain two information
-                  stored in two different columns: part name and part barcode.
-                  Here you can set which column of the .csv file you added
-                  contain those informations. If the column of your csv match
-                  the default column a green indicator will be put next to his
-                  name.
-                </div>
-                <Row
-                  className="m-3"
-                  style={{
-                    borderStyle: "solid",
-                    borderWidth: "1px",
-                    borderColor: "lightgray",
-                  }}
-                >
-                  <Col style={{ borderRight: "1px" }}>
-                    <div>Barcode column name</div>
-                    <hr />
-                    <div>
-                      <UncontrolledButtonDropdown>
-                        <DropdownToggle color="link" caret>
-                          <MatchComponent
-                            match={
-                              usedHeader.barcode === DEFAULT_HEADER_BARCODE
-                            }
-                          />{" "}
-                          {usedHeader.barcode}
-                        </DropdownToggle>
-                        <DropdownMenu>
-                          {headersName.map((x) => (
-                            <DropdownItem
-                              onClick={() =>
-                                setUsedHeader((prevState) => ({
-                                  ...prevState,
-                                  barcode: x,
-                                }))
-                              }
-                              key={x}
-                            >
-                              {x}
-                            </DropdownItem>
-                          ))}
-                        </DropdownMenu>
-                      </UncontrolledButtonDropdown>
-                    </div>
-                  </Col>
-                  <Col>
-                    <div>Part name column name</div>
-                    <hr />
-                    <div>
-                      <UncontrolledButtonDropdown>
-                        <DropdownToggle color="link" caret>
-                          <MatchComponent
-                            match={usedHeader.name === DEFAULT_HEADER_NAME}
-                          />{" "}
-                          {usedHeader.name}
-                        </DropdownToggle>
-                        <DropdownMenu>
-                          {headersName.map((x) => (
-                            <DropdownItem
-                              onClick={() =>
-                                setUsedHeader((prevState) => ({
-                                  ...prevState,
-                                  name: x,
-                                }))
-                              }
-                              key={x}
-                            >
-                              {x}
-                            </DropdownItem>
-                          ))}
-                        </DropdownMenu>
-                      </UncontrolledButtonDropdown>
-                    </div>
-                  </Col>
-                </Row>
-              </div>
-            ) : null}
-          </Collapse>
-        </ModalBody>
-        <ModalFooter>
-          <Button color="warning" onClick={toggleModal}>
-            Close
-          </Button>
-          <Button color="primary" disabled={fileInput === null} onClick={send}>
-            Submit
-          </Button>
-        </ModalFooter>
-      </Modal>
-    </span>
-  );
-};
+import { Button, Container } from "reactstrap";
 
 const PrintCell = ({ print, id }) => {
   const [number, setNumber] = React.useState(1);
@@ -405,31 +69,28 @@ const PrintCell = ({ print, id }) => {
   );
 };
 
+const helpRowRenderer = () => {
+  return (
+    <Container style={{ padding: "15px", textAlign: "center", color: "grey" }}>
+      {"There is no 'Parts' in the database."}
+      <br />
+      {"Click on the "}
+      <Button disabled color="light" className="mr-2">
+        📂
+      </Button>
+      {" button to import a '.csv'."}
+    </Container>
+  );
+};
+
 export default ({ parts }) => {
   const [filter, setFilter] = useState("");
-
-  const helpRowRenderer = () => {
-    return (
-      <Container
-        style={{ padding: "15px", textAlign: "center", color: "grey" }}
-      >
-        {"There is no 'Parts' in the database."}
-        <br />
-        {"Click on the "}
-        <Button disabled color="light" className="mr-2">
-          📂
-        </Button>
-        {" button to import a '.csv'."}
-      </Container>
-    );
-  };
-
   const filtered = parts.filter(filter);
 
   return (
     <div>
       <CardHeaderSearch value={filter} onChange={(x) => setFilter(x)}>
-        <PartImportModal add={parts.add} />
+        <ImportModal add={parts.add} />
         <BulkImportModal importCSV={parts.importCSV} />
         <RefreshButton refresh={parts.fetch}>
           Reload the part data
